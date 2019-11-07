9eba06fbf9b2e0589f5670a153738a6716d36677
Nico-on <Nico-on@users.noreply.github.com>
2019-11-07 14:23:17 +0200

    k

---
+++ eduvantaa-master/aivot.js
@@ -0,0 +1,38 @@
+function laatikkoFunktio(boxNumber){
+    alert("Nyt vaihdetaan laatikko"+ boxNumber + ":n sisältöä!");
+
+    if (boxNumber == 1){
+        document.getElementById("laatikko1").innerHTML = "<h1 class='uusiOtsikko1'>Suomi on Suomalaisille !</h1>" + 
+        "<p>PERKELE...</p>";
+        document.getElementById("laatikko1").style.lineHeight = "50px";
+    }
+    else if (boxNumber == 2){
+        document.getElementById("laatikko2").innerHTML = "<table style='width:100%'>" +
+        "<tr><td>maanantai</td></tr>" +
+        "<tr><td>8-15</td></tr>"  +
+        "<tr><td>Tiistai</td></tr>" +
+        "<tr><td>8-16</td></tr>" +
+        "<tr><td>keskiviikko</td></tr>" +
+        "<tr><td>8-13</td></tr>" +
+        "<tr><td>Torstai</td></tr>" +
+        "<tr><td>8-14</td></tr>" +
+        "<tr><td>Perjantai</td></tr>" +
+        "<tr><td>8-14</td></tr>" +
+        "</table>";
+    }
+    else if (boxNumber == 3){ 
+        document.getElementById("laatikko3").innerHTML = "<img src='raccon.jpg'>";
+
+        
+    }
+    
+}
+function yhteystietoFunktio() {
+    var x = document.getElementById("yhteystietolomake");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+      text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("yhteystiedotTuloste").innerHTML = text;
+  }
